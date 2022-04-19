899b47d45b864b8419c48a60f5a41d81141dc10e
Soura Gopal Sethy <soura.gopal@incture.com>
2022-04-19 13:15:04 +0530

    first commit

---
+++ src/main/resources/script/script2.groovy
@@ -0,0 +1,34 @@
+/*
+ The integration developer needs to create the method processData 
+ This method takes Message object of package com.sap.gateway.ip.core.customdev.util 
+which includes helper methods useful for the content developer:
+The methods available are:
+    public java.lang.Object getBody()
+	public void setBody(java.lang.Object exchangeBody)
+    public java.util.Map<java.lang.String,java.lang.Object> getHeaders()
+    public void setHeaders(java.util.Map<java.lang.String,java.lang.Object> exchangeHeaders)
+    public void setHeader(java.lang.String name, java.lang.Object value)
+    public java.util.Map<java.lang.String,java.lang.Object> getProperties()
+    public void setProperties(java.util.Map<java.lang.String,java.lang.Object> exchangeProperties) 
+    public void setProperty(java.lang.String name, java.lang.Object value)
+    public java.util.List<com.sap.gateway.ip.core.customdev.util.SoapHeader> getSoapHeaders()
+    public void setSoapHeaders(java.util.List<com.sap.gateway.ip.core.customdev.util.SoapHeader> soapHeaders) 
+       public void clearSoapHeaders()
+ */
+import com.sap.gateway.ip.core.customdev.util.Message;
+import java.util.HashMap;
+def Message processData(Message message) {
+    def body = message.getBody(java.lang.String) as String;
+def messageLog = messageLogFactory.getMessageLog(message);
+//def oException = message.getProperty(‘CamelExceptionCaught’);
+
+def get = new URL("https://api.coindesk.com/v1/bpi/currentprice.json").openConnection();
+//get.setRequestProperty(‘Authorization’, ‘Basic ‘ + ‘Username:password’.bytes.encodeBase64().toString())
+def getRC = get.getResponseCode();
+
+if(getRC.equals(200)) {
+//messageLog.setStringProperty(“ResponsePayload-:”,getRC.toString());
+}
+message.setBody(get.getInputStream().getText());
+return message;
+}
